ce3c82948407a7f43b14ed005048fbef31b31065
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-04-10 01:00:14 +0200

    Implemented Categories demo

--- app/pages/Home/index.tsx
+++ app/pages/Home/index.tsx
@@ -2,12 +2,43 @@
  * Home
  */
 
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 import { Helmet } from 'react-helmet';
+import { compose, graphql } from 'react-apollo';
+// Components
+import Dropdown from '../../components/Dropdown/Loadable';
+// Containers
+import Categories from '../../containers/GetCategories/Loadable';
+// Libraries
+import { makeDebugger } from '../../lib';
 // Styles
 import Wrapper from './styles';
 
-class Home extends PureComponent {
+const debug = makeDebugger('Home');
+
+class Home extends Component<{}, IState> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      category: {
+        name: '',
+        value: '-1',
+      },
+    };
+  }
+
+  public componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        category: {
+          name: 'Automotive Industry',
+          value: 'cje5zpib903lv0164blori54s',
+        },
+      });
+    }, 5000);
+  }
+
   public render() {
     return (
       <Wrapper>
@@ -15,9 +46,35 @@ class Home extends PureComponent {
           <title>Home</title>
           <meta name="description" content="Description of Home" />
         </Helmet>
+        <Categories
+          render={
+            ({ hasCategories, staleCategories }) => {
+              const categories = (hasCategories && staleCategories.map((category) => {
+                const { node } = category;
+
+                return {
+                  name: node.name,
+                  value: node.id,
+                };
+              })) || [];
+
+              return (
+                <Dropdown
+                  selected={this.state.category}
+                  options={categories}
+                  onChange={(data) => debug({ data })}
+                />
+              );
+            }
+          }
+        />
       </Wrapper>
     );
   }
+}
+
+interface IState {
+  category: object;
 }
 
 export default Home;
